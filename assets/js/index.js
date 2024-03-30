07cf63f0f082aca3361233d3dd1451e62f7aa951
MariFer14 <ferarenilla@gmail.com>
2024-03-30 12:04:11 -0500

    Agregando funcionalidad para el valor total

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,15 @@
-total = 0;
 precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio;
+let valorTotal = document.querySelector(".valor-total");
 
 function Suma() {
   cantidad = document.querySelector(".cantidad");
   aumentar = Number(cantidad.innerHTML);
   aumentar = aumentar + 1;
   cantidad.innerHTML = aumentar;
+  valorTotal.innerHTML = aumentar * precio;
 }
 
 function Resta() {
@@ -16,4 +17,5 @@ function Resta() {
   disminuir = Number(cantidad.innerHTML);
   disminuir = disminuir - 1;
   cantidad.innerHTML = disminuir;
+  valorTotal.innerHTML = disminuir * precio;
 }
